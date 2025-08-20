4868772aa78ef135a663ef5881c2fd95ebfc4077
Ritik <ritikjain121299@gmail.com>
2025-08-20 03:40:24 +0000

    added a_star

--- src/planner/src/test.cpp
+++ src/planner/src/test.cpp
@@ -6,6 +6,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
 #include "nav_msgs/msg/path.hpp"
+#include "a_star.h"
 
 using namespace std::chrono_literals;
 
@@ -15,10 +16,11 @@ using namespace std::chrono_literals;
 class MinimalPublisher : public rclcpp::Node
 {
 public:
-    MinimalPublisher(nav_msgs::msg::OccupancyGrid map)
-        : Node("map_publisher"), map_(map)
+    MinimalPublisher(nav_msgs::msg::OccupancyGrid map, nav_msgs::msg::Path path)
+        : Node("map_publisher"), map_(map), path_(path)
     {
-        map_publisher_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("topic", 10);
+        map_publisher_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/map", 10);
+        path_publisher_ = this->create_publisher<nav_msgs::msg::Path>("/path", 10);
         timer_ = this->create_wall_timer(
             500ms, std::bind(&MinimalPublisher::timer_callback, this));
     }
@@ -27,10 +29,13 @@ private:
     void timer_callback()
     {
         map_publisher_->publish(map_);
+        path_publisher_->publish(path_);
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_publisher_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher_;
     nav_msgs::msg::OccupancyGrid map_;
+    nav_msgs::msg::Path path_;
 };
 
 int main(int argc, char *argv[])
@@ -43,7 +48,35 @@ int main(int argc, char *argv[])
     map.info.height = 10;
     map.data.resize(map.info.width * map.info.height, 0); // Initialize with zeros
 
-    rclcpp::spin(std::make_shared<MinimalPublisher>(map));
+    AStar a_star(map.info.resolution, 8.0f);
+    a_star.setMap(map.data, map.info.width, map.info.height);
+    a_star.setStartNode(0.0f, 0.0f, 0.0f);
+    a_star.setEndNode(9.0f, 9.0f, 0.0f);
+
+    nav_msgs::msg::Path path;
+
+    std::vector<Point> path_points;
+    if (a_star.getPath(path_points))
+    {
+        path.header.frame_id = "map";
+        for (const auto &point : path_points)
+        {
+            geometry_msgs::msg::PoseStamped pose;
+            pose.header.frame_id = "map";
+            pose.pose.position.x = point.x;
+            pose.pose.position.y = point.y;
+            pose.pose.orientation.z = sin(point.theta / 2.0);
+            pose.pose.orientation.w = cos(point.theta / 2.0);
+            std::cout << "Path point: (" << point.x << ", " << point.y << ", " << point.theta << ")" << std::endl;
+            path.poses.push_back(pose);
+        }
+    }
+    else
+    {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "No path found");
+    }
+
+    rclcpp::spin(std::make_shared<MinimalPublisher>(map, path));
     rclcpp::shutdown();
     return 0;
 }
